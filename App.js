40736d98ea67267ea7d36002f3724ef72cd0cbbc
Zhenghao Lou <rhzx3519@gmail.com>
2024-04-03 23:45:49 +1100

    init

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NativeModules, Button} from 'react-native';
+
+const {CalendarModule} = NativeModules;
+
+const NewModuleButton = () => {
+  const onPress = () => {
+    CalendarModule.createCalendarEvent('testName', 'testLocation');
+  };
+
+  return (
+    <Button
+      title="Click to invoke your native module!"
+      color="#841584"
+      onPress={onPress}
+    />
+  );
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <NewModuleButton />
     </View>
   );
 }
