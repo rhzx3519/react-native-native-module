4b35365736107c6e78045ab481192ad87f3cbf6a
Zhenghao Lou <rhzx3519@gmail.com>
2024-04-04 00:11:40 +1100

    fetch device id

--- App.js
+++ App.js
@@ -1,29 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {NativeModules, Button} from 'react-native';
+import { Button } from 'react-native';
+import CalendarModule from './CalendarModule';
+import { useEffect, useState } from 'react';
 
-const {CalendarModule} = NativeModules;
-
-const NewModuleButton = () => {
-  const onPress = () => {
-    CalendarModule.createCalendarEvent('testName', 'testLocation');
-  };
-
-  return (
-    <Button
-      title="Click to invoke your native module!"
-      color="#841584"
-      onPress={onPress}
-    />
-  );
-};
 
 export default function App() {
+  const [error, setError] = useState('')
+  const [deviceId, setDeviceId] = useState('')
+  const [deviceAttribute, setDeviceAttribute] = useState('')
+
+  useEffect(() => {
+    CalendarModule.getDeviceID(
+      deviceId => {
+        setDeviceId(deviceId)
+      },
+      error => {
+        setError(error)
+      }
+    )
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-      <NewModuleButton />
+      <Text>DeviceId: {deviceId}</Text>
+      <Text style={{ color: 'red' }}>{error}</Text>
     </View>
   );
 }
